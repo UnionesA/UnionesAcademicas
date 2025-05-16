fdbc32c08b90694f52027553701a98e48eb43f2f
UnionesA <dianamnssua@gmail.com>
2025-05-16 06:38:26 +0000

    new file:   ControladorLogin.php 	new file:   ControladorRegistro.php 	modified:   Index.html 	modified:   Registro.html

---
+++ ControladorLogin.php
@@ -0,0 +1,27 @@
+<?php 
+$servername="localhost"; 
+$username="root"; 
+$password=""; 
+$dbname="registro"; 
+//crear conexion con la base de datos 
+$conn=new mysqli ($servername, $username, $password, $dbname); 
+// verificar conexion 
+if ($conn->connect_error) { 
+    die("conexion fallida:". $conn->connect_error); 
+} 
+//obtener datos de formulario 
+$usuario=$_POST['usuario']; 
+$clave=$_POST['clave']; 
+//verificar usuario y contraseña
+$sql = "SELECT * FROM `informacion` WHERE usuario='$usuario' AND clave='$clave'";
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    // Redirige a la página de inicio (cambia 'bienvenido.html' por la que desees)
+    header("Location: Uniones-Academicas.html");
+    exit();
+} else { 
+    echo 'Usuario o contraseña incorrecta'; 
+} 
+$conn->close(); 
+?>
---
+++ ControladorRegistro.php
@@ -0,0 +1,29 @@
+<?php 
+$servername="localhost"; 
+$username="root"; 
+$password=""; 
+$dbname="registro"; 
+//crear conexion con la base de datos 
+$conn=new mysqli ($servername, $username, $password, $dbname); 
+// verificar conexion 
+if ($conn->connect_error) 
+{ 
+    die("conexion fallida:". $conn->connect_error); 
+} 
+//obtener datos de formulario 
+$usuario=$_POST['usuario']; 
+$clave=$_POST['clave']; 
+//insertar datos en la base de datos (corregido el SQL)
+$sql="INSERT INTO informacion (usuario, clave) VALUES ('$usuario', '$clave')"; 
+if ($conn->query($sql)===TRUE) 
+{
+    // Redirigir a la página de inicio de sesión
+    header("Location:Index.html");
+    exit();
+} 
+else 
+{ 
+    echo 'Registro incorrecto'; 
+} 
+$conn->close(); 
+?>
